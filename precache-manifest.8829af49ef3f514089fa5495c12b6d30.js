89bde4c837f5fd0b99557f5d399d37fdc13a11a7
AndreFontaine <andres.gonzalez.ud@gmail.com>
2020-05-22 12:57:46 +0100

    Updates

---
+++ precache-manifest.8829af49ef3f514089fa5495c12b6d30.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c95f1a0319a848c8a1b58deaa22524",
+    "url": "/social-media-dashboard-react/index.html"
+  },
+  {
+    "revision": "7bcf38349c0f5b9dae06",
+    "url": "/social-media-dashboard-react/static/css/main.3571076e.chunk.css"
+  },
+  {
+    "revision": "1ffce4fd446784a4d4c2",
+    "url": "/social-media-dashboard-react/static/js/2.60ba9977.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard-react/static/js/2.60ba9977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bcf38349c0f5b9dae06",
+    "url": "/social-media-dashboard-react/static/js/main.a3048812.chunk.js"
+  },
+  {
+    "revision": "09a49b21d6a219588403",
+    "url": "/social-media-dashboard-react/static/js/runtime-main.eab1815c.js"
+  },
+  {
+    "revision": "69a5c77f51678c145d64af371ff73b22",
+    "url": "/social-media-dashboard-react/static/media/icon-down.69a5c77f.svg"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/social-media-dashboard-react/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/social-media-dashboard-react/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/social-media-dashboard-react/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "95f916a549511a0912328d5626522d2c",
+    "url": "/social-media-dashboard-react/static/media/icon-up.95f916a5.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/social-media-dashboard-react/static/media/icon-youtube.8499da12.svg"
+  }
+]);
